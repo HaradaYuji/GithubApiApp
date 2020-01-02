7a6a1636ee89220a663604d4242e45ea0a5cdd61
原田悠嗣 <bikkirisaburou@gmail.com>
2020-01-02 23:35:22 +0900

    Add Decodable

--- GithubApiApp/Entities/Oganization.swift
+++ GithubApiApp/Entities/Oganization.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Oganization {
+struct Oganization: Decodable {
     let id: Int
     let login: String
 }
--- GithubApiApp/Entities/Repository.swift
+++ GithubApiApp/Entities/Repository.swift
@@ -8,9 +8,16 @@
 
 import Foundation
 
-struct Repository {
+struct Repository: Decodable {
     let id: Int
     let name: String
     let fullName: String
     let owner: Oganization
+
+    enum CodingKeys : String, CodingKey {
+        case id
+        case name
+        case fullName = "full_name"
+        case owner
+    }
 }
