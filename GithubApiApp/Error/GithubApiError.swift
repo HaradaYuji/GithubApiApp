99e1c1247389734bb8ba9b177fac77afa64c9663
原田悠嗣 <bikkirisaburou@gmail.com>
2020-01-03 00:03:25 +0900

    Add ApiError

---
+++ GithubApiApp/Error/GithubApiError.swift
@@ -0,0 +1,13 @@
+//
+//  GithubApiError.swift
+//  GithubApiApp
+//
+//  Created by 原田悠嗣 on 2020/01/02.
+//  Copyright © 2020 原田悠嗣. All rights reserved.
+//
+
+import Foundation
+
+struct GithubApiError : Decodable, Error {
+    let message: String
+}
---
+++ GithubApiApp/Error/GithubCliantError.swift
@@ -0,0 +1,18 @@
+//
+//  GithubCliantError.swift
+//  GithubApiApp
+//
+//  Created by 原田悠嗣 on 2020/01/02.
+//  Copyright © 2020 原田悠嗣. All rights reserved.
+//
+
+import Foundation
+
+enum GithubCliantError: Error {
+    // 通信に失敗
+    case connectionError(Error)
+    // レスポンスの解釈に失敗
+    case responseError(Error)
+    // APIからエラーレスポンスを受け取った
+    case apiError(GithubApiError)
+}
