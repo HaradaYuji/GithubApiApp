414f2709a7788949543abc7d34fbc7663d6e3157
原田悠嗣 <bikkirisaburou@gmail.com>
2020-01-02 23:15:36 +0900

    Add Organization

---
+++ GithubApiApp/Entities/Oganization.swift
@@ -0,0 +1,14 @@
+//
+//  Oganization.swift
+//  GithubApiApp
+//
+//  Created by 原田悠嗣 on 2020/01/02.
+//  Copyright © 2020 原田悠嗣. All rights reserved.
+//
+
+import Foundation
+
+struct Oganization {
+    let id: Int
+    let login: String
+}
