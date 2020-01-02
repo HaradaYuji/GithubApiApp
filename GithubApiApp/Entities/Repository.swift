26e9c97bd9d19b5ba7adb304cfdb6b3cfc3ff737
原田悠嗣 <bikkirisaburou@gmail.com>
2020-01-02 23:28:04 +0900

    Add Repository

---
+++ GithubApiApp/Entities/Repository.swift
@@ -0,0 +1,16 @@
+//
+//  Repository.swift
+//  GithubApiApp
+//
+//  Created by 原田悠嗣 on 2020/01/02.
+//  Copyright © 2020 原田悠嗣. All rights reserved.
+//
+
+import Foundation
+
+struct Repository {
+    let id: Int
+    let name: String
+    let fullName: String
+    let owner: Oganization
+}
